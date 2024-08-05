c64576ede03c8750ba5b3b85d98389fc84fac658
Hardik Mehta <hardikm9850@gmail.com>
2024-08-05 11:15:31 +0530

    add : routes for creating user

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
-	database "go_bikes/internal"
+	"go_bikes/internal/database"
+	"go_bikes/internal/handlers"
+	"go_bikes/internal/repository"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func main() {
-	err := database.Connect()
+	db := prepareDatabase()
+	prepareConnection(db)
+}
+
+func prepareDatabase() (db *gorm.DB) {
+	db, err := database.Connect()
 	if err != nil {
 		fmt.Print(err)
 		return
 	}
+	err = database.Migrate(db)
+	if err != nil {
+		log.Fatal("failed to migrate database", err)
+	}
+	return db
+}
+
+func prepareConnection(db *gorm.DB) {
+	userRepository := repository.NewUserRepository()
+	userHandler := handlers.NewUserHandler(userRepository, db)
+
+	// init Gin router
+	r := gin.Default()
+	r.POST("/users", userHandler.CreateUser)
+	r.GET("/users", userHandler.GetUsers)
+
+	r.Run(":8080")
 }
